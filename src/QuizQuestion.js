ed7a15ba9bffa0be01c3e85c49bf5265172bcb26
unknown <nahuel307@gmail.com>
2020-01-28 19:52:09 -0300

    Adding event chain

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,11 @@ import React,{Component} from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component{
+  handleClick(buttonText){
+    if(buttonText===this.props.quiz_question.answer){
+      this.props.showNextQuestionHandler()
+    }
+  }
   render(){
       return(
           <main>
@@ -9,8 +14,11 @@ class QuizQuestion extends Component{
               <p>{this.props.quiz_question.instruction_text}</p>
             </section>
             <section className='buttons'>
-            <ul>
-                <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+            <ul >
+                {this.props.quiz_question.answer_options.map((answer_option,index)=>{
+                    return <QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={index} button_text={answer_option} />
+                })}
+
             </ul>
             </section>
           </main>
