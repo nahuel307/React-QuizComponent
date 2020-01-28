4ac1c773ee713c1878bbfe0e5e08a136ec178517
unknown <nahuel307@gmail.com>
2020-01-28 20:09:14 -0300

    Adding error message

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,14 +2,27 @@ import React,{Component} from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component{
-  handleClick(buttonText){
-    if(buttonText===this.props.quiz_question.answer){
-      this.props.showNextQuestionHandler()
+  constructor(props){
+    super(props);
+    this.state={
+      incorrectAnswer:false
     }
   }
+
+  handleClick(buttonText){
+    if(buttonText===this.props.quiz_question.answer){
+      this.props.showNextQuestionHandler();
+      this.setState((state)=>{return({incorrectAnswer:false} ) })
+    }
+    else {
+      this.setState((state)=>{return({incorrectAnswer:true} ) })
+    }
+  }
+
   render(){
       return(
           <main>
+            {this.state.incorrectAnswer?<p className='error'>Sorry, that's not right</p>:null}
             <section>
               <p>{this.props.quiz_question.instruction_text}</p>
             </section>
@@ -18,7 +31,6 @@ class QuizQuestion extends Component{
                 {this.props.quiz_question.answer_options.map((answer_option,index)=>{
                     return <QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={index} button_text={answer_option} />
                 })}
-
             </ul>
             </section>
           </main>
